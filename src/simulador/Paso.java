2e11ab6c2c22791be146df5316fd2a1580e3ce4a
etorrielli <e.torrielli@gmail.com>
2017-05-16 01:21:09 -0300

    PantallaPrueba

--- src/simulador/Paso.java
+++ src/simulador/Paso.java
@@ -5,13 +5,18 @@
  */
 package simulador;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author Edgar
  */
 public class Paso {
     
-    private static enum Evento {
+    public static enum Evento {
                                 INICIO,
                                 LLEGADA,
                                 FIN_A,
@@ -19,31 +24,34 @@ public class Paso {
                                 FIN_ESCUCHAR,
                                 FIN_COMPRAR
                                 }
-    private static enum Actividad {
+    public static enum Actividad {
                                 MIRAR,
                                 CONSULTAR
                                 }
-    private static enum Consulta {
+    public static enum Consulta {
                                 POR_COMPRA,
                                 POR_ESCUCHA
                                 }
-    private static enum Decision {
+    public static enum Decision {
                                 COMPRAR,
                                 NO_COMPRAR
                                 }
-    private static enum Estado {
+    public static enum Estado {
                                 LIBRE,
                                 OCUPADO
                                 }
-    private static enum Cajero {
+    public static enum Cajero {
                                 A,
                                 B
                                 }
     
-    private long hora;
+    private double hora;
     private int rndLlegada;
-    private long tEntreLleg;
-    private long proxLlegada;
+    private double tEntreLleg;
+    private double proxLlegada;
+    
+    private double finA;
+    private double finB;
     
     private int nroCliLlegada;
     private int nroCliFinA;
@@ -57,11 +65,265 @@ public class Paso {
     private int rndActividad;
     private int rndTipoConsulta;
     private int rndEscucha;
-    private long finEscucha;
+    private double finEscucha;
     private int rndDecision;
-    private long finCompra;
+    private double finCompra;
     
     private Evento evento;
     private String eventoNumerado;
     private Evento menorEvento;
+    
+    private String pEventoNumerado;
+    private List<Double> eventosOrdenados = new ArrayList<Double>();
+    private int mayorIndice;
+    private List<Integer> indicesOrdenados = new ArrayList<Integer>();
+    
+    public void Paso(){
+        
+    }
+    
+    public void Paso(double hora, Evento evento){
+        this.hora = hora;
+        this.evento = evento;
+    }
+    
+    public List<Double> getRelojMenorEvento(){
+        
+        if(proxLlegada > 0){
+            eventosOrdenados.add(proxLlegada);
+        }
+        if(finA > 0){
+            eventosOrdenados.add(finA);
+        }
+        if(finB > 0){
+            eventosOrdenados.add(finB);
+        }
+        if(finEscucha > 0){
+            eventosOrdenados.add(finEscucha);
+        }
+        if(finCompra > 0){
+            eventosOrdenados.add(finCompra);
+        }
+        
+        Collections.sort(eventosOrdenados);
+        return eventosOrdenados;
+    }
+
+    public double getHora() {
+        return hora;
+    }
+
+    public void setHora(double hora) {
+        this.hora = hora;
+    }
+
+    public int getRndLlegada() {
+        return rndLlegada;
+    }
+
+    public void setRndLlegada(int rndLlegada) {
+        this.rndLlegada = rndLlegada;
+    }
+
+    public double gettEntreLleg() {
+        return tEntreLleg;
+    }
+
+    public void settEntreLleg(double tEntreLleg) {
+        this.tEntreLleg = tEntreLleg;
+    }
+
+    public double getProxLlegada() {
+        return proxLlegada;
+    }
+
+    public void setProxLlegada(double proxLlegada) {
+        this.proxLlegada = proxLlegada;
+    }
+
+    public double getFinA() {
+        return finA;
+    }
+
+    public void setFinA(double finA) {
+        this.finA = finA;
+    }
+
+    public double getFinB() {
+        return finB;
+    }
+
+    public void setFinB(double finB) {
+        this.finB = finB;
+    }
+
+    public int getNroCliLlegada() {
+        return nroCliLlegada;
+    }
+
+    public void setNroCliLlegada(int nroCliLlegada) {
+        this.nroCliLlegada = nroCliLlegada;
+    }
+
+    public int getNroCliFinA() {
+        return nroCliFinA;
+    }
+
+    public void setNroCliFinA(int nroCliFinA) {
+        this.nroCliFinA = nroCliFinA;
+    }
+
+    public int getNroCliFinB() {
+        return nroCliFinB;
+    }
+
+    public void setNroCliFinB(int nroCliFinB) {
+        this.nroCliFinB = nroCliFinB;
+    }
+
+    public int getNroCliFinEscucha() {
+        return nroCliFinEscucha;
+    }
+
+    public void setNroCliFinEscucha(int nroCliFinEscucha) {
+        this.nroCliFinEscucha = nroCliFinEscucha;
+    }
+
+    public int getNroCliFinCompra() {
+        return nroCliFinCompra;
+    }
+
+    public void setNroCliFinCompra(int nroCliFinCompra) {
+        this.nroCliFinCompra = nroCliFinCompra;
+    }
+
+    public Estado getEstadoA() {
+        return estadoA;
+    }
+
+    public void setEstadoA(Estado estadoA) {
+        this.estadoA = estadoA;
+    }
+
+    public Estado getEstadoB() {
+        return estadoB;
+    }
+
+    public void setEstadoB(Estado estadoB) {
+        this.estadoB = estadoB;
+    }
+
+    public int getCola() {
+        return cola;
+    }
+
+    public void setCola(int cola) {
+        this.cola = cola;
+    }
+
+    public int getRndActividad() {
+        return rndActividad;
+    }
+
+    public void setRndActividad(int rndActividad) {
+        this.rndActividad = rndActividad;
+    }
+
+    public int getRndTipoConsulta() {
+        return rndTipoConsulta;
+    }
+
+    public void setRndTipoConsulta(int rndTipoConsulta) {
+        this.rndTipoConsulta = rndTipoConsulta;
+    }
+
+    public int getRndEscucha() {
+        return rndEscucha;
+    }
+
+    public void setRndEscucha(int rndEscucha) {
+        this.rndEscucha = rndEscucha;
+    }
+
+    public double getFinEscucha() {
+        return finEscucha;
+    }
+
+    public void setFinEscucha(double finEscucha) {
+        this.finEscucha = finEscucha;
+    }
+
+    public int getRndDecision() {
+        return rndDecision;
+    }
+
+    public void setRndDecision(int rndDecision) {
+        this.rndDecision = rndDecision;
+    }
+
+    public double getFinCompra() {
+        return finCompra;
+    }
+
+    public void setFinCompra(double finCompra) {
+        this.finCompra = finCompra;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+
+    public String getEventoNumerado() {
+        return eventoNumerado;
+    }
+
+    public void setEventoNumerado(String valor) {
+        this.eventoNumerado = eventoNumerado + " " + valor;
+    }
+
+    public Evento getMenorEvento() {
+        return menorEvento;
+    }
+
+    public void setMenorEvento(Evento menorEvento) {
+        this.menorEvento = menorEvento;
+    }
+
+    public String getpEventoNumerado() {
+        return pEventoNumerado;
+    }
+
+    public void setpEventoNumerado(String pEventoNumerado) {
+        this.pEventoNumerado = pEventoNumerado;
+    }
+
+    public List<Double> getEventosOrdenados() {
+        return eventosOrdenados;
+    }
+
+    public void setEventosOrdenados(List<Double> eventosOrdenados) {
+        this.eventosOrdenados = eventosOrdenados;
+    }
+
+    public int getMayorIndice() {
+        
+        indicesOrdenados.add(nroCliFinA);
+        indicesOrdenados.add(nroCliFinB);
+        indicesOrdenados.add(nroCliFinCompra);
+        indicesOrdenados.add(nroCliFinEscucha);
+        
+        Collections.sort(indicesOrdenados);
+        return indicesOrdenados.get(3) + 1;
+    }
+
+    public void setMayorIndice(int mayorIndice) {
+        this.mayorIndice = mayorIndice;
+    }
+    
+    
+    
 }
--- src/simulador/Simulador.java
+++ src/simulador/Simulador.java
@@ -5,13 +5,26 @@
  */
 package simulador;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import java.util.Arrays;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import static simulador.Paso.*;
 
 /**
  *
@@ -19,34 +32,94 @@ import javafx.stage.Stage;
  */
 public class Simulador extends Application {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Simular");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                simular();
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("SuperSimulador");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    private Paso paso;
+    private Pasos pasos;
+
+    public Pasos getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(Pasos pasos) {
+        this.pasos = pasos;
     }
     
     public static void main(String[] args) {
         launch(args);
     }
     
-    public void simular(){
-        System.out.println("Hello World!");
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        //SIMULAR
+        crearPasos(0, 600);
+        
+        //BOTON
+        /*Button btn = new Button();
+        btn.setText("Simular");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                crearPasos(0, 600);
+                
+
+            }
+        });*/
+        
+        //TABLA
+        TableView<Paso> tv = new TableView<Paso>();
+        ObservableList<Paso> people = FXCollections.observableArrayList(getPasos().getLstPaso());
+        TableColumn<Paso, String> typeCol = new TableColumn<>("evento");
+        typeCol.setCellValueFactory(
+            new PropertyValueFactory<>("evento")
+        );
+        tv.getColumns().add(typeCol);
+        tv.setItems(people);
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(tv);
+        //root.getChildren().add(btn);
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("SuperSimulador");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+    
+    
+    
+    public void crearPasos(long horaInicio, long horaFin){
+        System.out.println("crearPasos!");
+        
+        Pasos pasos = new Pasos();
+        Paso paso = new Paso();
+        paso.setHora(horaInicio);
+        paso.setEvento(Evento.INICIO);
+        paso.setRndLlegada(getGeneradorLlegada());
+        paso.settEntreLleg(obtenerTiempoLLegada(paso.getRndLlegada()));
+        paso.setProxLlegada(horaInicio + paso.gettEntreLleg());
+        System.out.println("setProxLlegada: " + paso.getProxLlegada());
+        
+        List<Paso> lstPasos = new ArrayList<Paso>();
+        lstPasos.add(paso);
+        pasos = new Pasos();
+        pasos.setLstPaso(lstPasos);
+        setPasos(pasos);
+        
+    }
+
+    public int getGeneradorLlegada() {
+        return ThreadLocalRandom.current().nextInt(0, 100 + 1);
+    }
+
+    public double obtenerTiempoLLegada(int rnd){
+        //calcular
+        System.out.println("rnd: " + rnd);
+        double a = (double) rnd / 100 * 4 + 5;
+        //double a = (double) rnd/10000;
+        System.out.println("a: " + a);
+        return a;
+    }
+    
+    
     
 }
